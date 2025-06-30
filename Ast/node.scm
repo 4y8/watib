ced5513c729d725f3f4c2cfb81efd58ef4e0a8dc
ayb <ayb@3hg.fr>
2025-06-30 22:02:19 +0200

    Minor update.

--- Ast/node.scm
+++ Ast/node.scm
@@ -1,7 +1,12 @@
 (module ast_node
-   (export (class instruction::object
+   (export (class func::object
+              body::block
+              locals::pair-nil)
+
+           (class instruction::object
               intype::pair-nil
               outtype::pair-nil
+              parent::func
               opcode::symbol)
 
            (abstract-class parameter)
@@ -9,12 +14,23 @@
            (class label::parameter
               idx::bint)
 
+           (class variable::parameter
+              idx::bint)
+
+           (class type::parameter
+              type)
+
            (class one-arg::instruction
               x::parameter)
 
            (class two-args::instruction
               x::parameter
               y::parameter)
+
+           (class three-args::instruction
+              x::parameter
+              y::parameter
+              z::parameter)
 
            (class br_table::instruction
               default::bint
@@ -32,21 +48,18 @@
            (class loop::instruction
               body::block)
 
-           (abstract-class catch-branch::object)
+           (class catch-branch::object
+              label::bint)
 
            (class catch::catch-branch
-              label::bint
               tagidx::bint)
 
            (class catch_ref::catch-branch
-              label::bint
               tagidx::bint)
 
-           (class catch_all::catch-branch
-              label::bint)
+           (class catch_all::catch-branch)
 
-           (class catch_all_ref::catch-branch
-              label::bint)
+           (class catch_all_ref::catch-branch)
 
            (class try_table::instruction
               catches::pair-nil
--- Makefile
+++ Makefile
@@ -1,16 +1,19 @@
+FLAGS:=-I Ast
+
+
 all: was watib
 
-was: was.o leb128.o opcodes.sch
-	bigloo was.o leb128.o -o was -O3 -unsafe
+was: was.o leb128.o
+	bigloo $(FLAGS) was.o leb128.o -o was -O3 -unsafe
 
-watib: Val/validate.o Ast/node.o
-	bigloo Val/validate.o -o watib -O3 -unsafe
+watib: Val/validate.o Ast/node.o Opt/TestBr/walk.o
+	bigloo $(FLAGS) Val/validate.o -o watib -O3 -unsafe
 
 Val/validate.o: Val/validate.scm type-abbreviations.sch Val/numtypes.sch Val/vectypes.sch Val/instruction-types.sch Val/constant-instructions.sch Val/absheaptypes.sch
-	bigloo -c Val/validate.scm -o Val/validate.o -O3 -unsafe
+	bigloo -c $(FLAGS) Val/validate.scm -o Val/validate.o -O3 -unsafe
 
 %.o : %.scm
-	bigloo -c $< -o $@ -O3 -unsafe
+	bigloo -c $(FLAGS) $< -o $@ -O3 -unsafe
 
 rapport.pdf: rapport.tex
 	latexmk -pdf rapport.tex
