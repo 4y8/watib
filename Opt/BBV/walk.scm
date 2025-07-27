be53044269f006fe819b26b20aa9d25a46a28e40
omelancon <ol.melancon@gmail.com>
2025-07-27 10:07:20 +0200

    methods for tracking context equivalence classes

--- Opt/BBV/walk.scm
+++ Opt/BBV/walk.scm
@@ -4,12 +4,20 @@
    (import (cfg_order "Opt/CFG/order.scm"))
    (import (type_type "Type/type.scm"))
 
-   (static (class context
-      registers::vector
-      stack::pair-nil))
+   (static (abstract-class location
+      pos::bint))
+
+   (static (class register::location))
+
+   (static (class onstack::location))
 
    (static (class types
       set::pair-nil))
+
+   (static (class context
+      registers::pair-nil
+      stack::pair-nil
+      equivalences::pair-nil))
 
    (static (class specialization
       origin::cfg-node
@@ -85,11 +93,128 @@
 (define (queue-peek queue) (if (queue-empty? queue) #f (caar queue)))
 
 ;; CONTEXT
-(define (make-empty-context nregisters::bint)
-   (instantiate::context (registers (make-vector nregisters #f)) (stack '())))
+(define-generic (on-popn::location location::location n::bint) location)
+(define-method (on-popn::onstack location::onstack n::bint)
+   (instantiate::onstack (pos (- (-> location pos) n))))
 
+(define-generic (on-pushn::location location::location n::bint) location)
+(define-method (on-pushn::onstack location::onstack n::bint)
+   (instantiate::onstack (pos (+ (-> location pos) n))))
+
+(define-generic (on-pop::location location::location) (on-popn location 1))
+(define-generic (on-push::location location::location) (on-pushn location 1))
+
+(define-generic (location-equal?::bool loc1::location loc2::location))
+(define-method (location-equal?::bool loc1::onstack loc2::location)
+   (and (isa? loc2 onstack) (= (-> loc1 pos) (-> loc2 pos))))
+(define-method (location-equal?::bool loc1::register loc2::location)
+   (and (isa? loc2 register) (= (-> loc1 pos) (-> loc2 pos))))
+
+(define (make-empty-context nregisters::bint)
+   (instantiate::context (registers '()) (stack '()) (equivalences '())))
+
+;; TODO
 (define (context-equal? ctx1 ctx) #t)
 
+(define (context-register-set::context context::context reg::bint type::types)
+   (let ((register-location (instantiate::register (pos reg))))
+      (with-access::context context (registers)
+         (context-remove-equivalence
+            (duplicate::context
+               context
+               (registers
+                  (cons
+                     (cons register-location type)
+                     (filter
+                        (lambda (reg-type)
+                           (with-access::register (car reg-type) (pos) (not (= pos reg))))
+                        registers))))
+            register-location))))
+
+(define (context-remove-equivalence::context context::context loc::location)
+   (duplicate::context
+      context
+      (equivalences
+         (cons
+            (list loc)
+            (filter pair?
+               (map
+                  (lambda (equiv) 
+                     (filter
+                        (lambda (equiv-loc-pair)
+                           (not (location-equal? loc (car equiv-loc-pair))))
+                        equiv))
+                  (-> context equivalences)))))))
+
+(define (equivalence-class-has?::bool equiv::pair-nil loc::location)
+   (let loop ((equiv equiv))
+      (and
+         (pair? equiv)
+         (or (location-equal? (car equiv) loc) (loop (cdr equiv))))))
+
+(define (context-get-equivalence-class::pair-nil context::context loc::location)
+   (let loop ((classes (-> context equivalences)))
+      (if (null? classes)
+          (error 'context-get-equivalence-class::pair-nil "no equivalence class for" loc)
+          (if (equivalence-class-has? (car classes) loc) (car classes) (loop (cdr classes))))))
+
+(define (context-add-equivalence::context context::context loc1::location loc2::location)
+   (duplicate::context
+      context
+      (equivalences
+         (let ((equiv1 (context-get-equivalence-class context loc1))
+               (equiv2 (context-get-equivalence-class context loc2)))
+            (cons
+               (if (eq? equiv1 equiv2) equiv1 (append equiv1 equiv2))
+               (filter
+                  (lambda (equiv) (not (or (eq? equiv equiv1) (eq? equiv equiv2))))
+                  (-> context equivalences)))))))
+
+(define (context-push::context context::context type::types)
+   (duplicate::context
+      context
+      (stack
+         (cons
+            (cons (instantiate::onstack (pos 0)) type)
+            (map
+               (lambda (onstack-type)  (cons (on-push (car onstack-type)) (cdr onstack-type)))
+               (-> context stack))))
+      (equivalences
+         (map
+            (lambda (equiv)
+               (map
+                  (lambda (loc-type) (cons (on-push (car loc-type)) (cdr loc-type)))
+                  equiv))
+            (-> context equivalences)))))
+
+(define (context-pop*::context context::context)
+   (duplicate::context
+      context
+      (stack
+         (cdr
+            (map
+               (lambda (onstack-type)  (cons (on-pop (car onstack-type)) (cdr onstack-type)))
+               (-> context stack))))
+      (equivalences
+         (filter pair?
+            (map
+               (lambda (equiv)
+                  (filter
+                     (lambda (loc-type)
+                        (let ((location (car loc-type)))
+                           (not (and
+                              (isa? location onstack)
+                              (< (with-access::onstack location (pos) pos) 0)))))
+                     (map
+                        (lambda (loc-type) (cons (on-pop (car loc-type)) (cdr loc-type)))
+                        equiv)))
+               (-> context equivalences))))))
+
+(define (context-pop context::context)
+   (values
+      (context-pop* context)
+      (cdar (-> context stack))))
+      
 ;; HELPERS
 (define (***NotImplemented*** name::symbol) (error "NotImplemented" name '?))
 
@@ -176,7 +301,7 @@
 (define-method (walk-jump instr::unconditional state::bbv-state context::context specialization::specialization)
    (with-access::unconditional instr (dst)
       (let ((jump-target (reach state dst context specialization)))
-         (instantiate::unconditional jump-target))))
+         (instantiate::unconditional (dst jump-target)))))
 
 (define-generic (walk-instr instr::instruction state::bbv-state context::context))
 
@@ -193,7 +318,7 @@
                   (vector-set! new-registers idx type)
                   (values
                      instr
-                     (instantiate::context (registers new-registers) (stack new-stack))))))
+                     (instantiate::context (registers new-registers) (stack new-stack) (equivalences '()))))))
          (i32.const
             (let ((type 'i32))
                (values
